43e8770515882c722d4dc883443ab12489411454
aahmadli14270 <84677778+aahmadli14270@users.noreply.github.com>
2022-12-29 12:52:13 +0400

    second version

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import "./App.css";
 import Header from "./Header-folder/Header";
 import MainContent from "./Main-content/MainContent";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+
+function InfoPage(props) {
+  return (
+    <div>
+      <MainContent items={props.sid}></MainContent>
+    </div>
+  );
+}
 
 function App() {
-  fetch("http://localhost:3500/items")
-    .then((res) => res.json())
-    .then((result) => console.log(result));
+  const [data, setData] = useState("0");
+
+  // let data = 0;
+  useEffect(() => {
+    fetch("http://localhost:3500/items")
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+        // console.log("DATA IS:", data);
+      });
+  }, []);
 
   return (
-    <div className="App">
-      <Header></Header>
-      <MainContent></MainContent>
-    </div>
+    <Router>
+      <div className="App">
+        <Header></Header>
+
+        <Link to="/InfoPage">
+          <p>{data[0].Series_Title}</p>
+        </Link>
+
+        {/* <MainContent items={data[0]}></MainContent>
+      <p>{data[0].ID}</p>
+      <p>{data[0].Series_Title}</p> */}
+
+        <Routes>
+          <Route
+            path="/InfoPage"
+            element={<InfoPage sid={data[0]}></InfoPage>}
+          ></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Main-content/MainContent.js
+++ src/Main-content/MainContent.js
@@ -1,26 +1,40 @@
 import "./MainContent.css";
 import "../general.css";
+import MainContentImage from "./MainContentImage/MainContentImage";
+import { useEffect, useState } from "react";
 
-function MainContent() {
+function MainContent(props) {
+  console.log("dwad", props.items);
+
+  const [data, setData] = useState("0");
+
+  useEffect(() => {
+    fetch(
+      `http://www.omdbapi.com/?i=tt3896198&apikey=13a29915&t=${props.items.Series_Title}
+      &y=${props.items.Released_Year}`
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+        // console.log("changed data", data[0]);
+      });
+  }, [props.items.Series_Title, props.items.Released_Year]);
+  console.log(data);
+
   return (
     <section className="main-container">
       <div className="grid-container">
         <div className="info-container">
-          <p>wad</p>
-          <p>wda</p>
-          <p>awdd</p>
-          <p>awdd</p>
-          <p>awdd</p>
-          <p>awdd</p>
-          <p>awdd</p>
+          <h1>{props.items.Series_Title}</h1>
+          <p>
+            {props.items.Released_Year} - {props.items.Runtime}
+          </p>
+          <p>{props.items.Genre}</p>
+          <p>{props.items.IMDB_Rating}</p>
+          <p>{props.items.Overview}</p>
+          <p>{props.items.Meta_score}</p>
         </div>
-        <div className="poster">
-          <img
-            className="poster-img"
-            src={require("../image/image.jpg")}
-            alt="wdad"
-          />
-        </div>
+        {data && <MainContentImage posterinfo={data}></MainContentImage>}
       </div>
     </section>
   );
