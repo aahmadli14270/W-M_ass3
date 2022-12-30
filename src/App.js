88070f21e03534632192d8613a0adeca4d812b2a
aahmadli14270 <84677778+aahmadli14270@users.noreply.github.com>
2022-12-30 20:53:44 +0400

    routing v2

--- src/App.js
+++ src/App.js
@@ -11,14 +11,6 @@ import {
   Navigate,
 } from "react-router-dom";
 
-function InfoPage(props) {
-  return (
-    <div>
-      <MainContent items={props.sid}></MainContent>
-    </div>
-  );
-}
-
 const test = 0;
 
 function App() {
@@ -35,29 +27,5 @@ function App() {
     </Router>
   );
 }
-
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Header></Header>
-
-//         <Link to="/InfoPage">
-//           <p>{data[0].Series_Title}</p>
-//         </Link>
-
-//         {/* <MainContent items={data[0]}></MainContent>
-//       <p>{data[0].ID}</p>
-//       <p>{data[0].Series_Title}</p> */}
-
-//         <Routes>
-//           <Route
-//             path="/InfoPage"
-//             element={<InfoPage sid={data[0]}></InfoPage>}
-//           ></Route>
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
 
 export default App;
--- src/InfoPage/InfoPage.js
+++ src/InfoPage/InfoPage.js
@@ -9,3 +9,35 @@ function InfoPage(props) {
 }
 
 export default InfoPage;
+
+//   return (
+//     <Router>
+//       <div className="App">
+//         <Header></Header>
+
+//         <Link to="/InfoPage">
+//           <p>{data[0].Series_Title}</p>
+//         </Link>
+
+//         {/* <MainContent items={data[0]}></MainContent>
+//       <p>{data[0].ID}</p>
+//       <p>{data[0].Series_Title}</p> */}
+
+//         <Routes>
+//           <Route
+//             path="/InfoPage"
+//             element={<InfoPage sid={data[0]}></InfoPage>}
+//           ></Route>
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// function InfoPage(props) {
+//   return (
+//     <div>
+//       <MainContent items={props.sid}></MainContent>
+//     </div>
+//   );
+// }
