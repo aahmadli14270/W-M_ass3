02d57defd08292724e65931d6ced4f8049beb966
aahmadli14270 <84677778+aahmadli14270@users.noreply.github.com>
2022-12-31 18:30:20 +0400

    infoPage v3

--- src/InfoPage/Main-content/MainContent.js
+++ src/InfoPage/Main-content/MainContent.js
@@ -2,7 +2,6 @@ import "./MainContent.css";
 import "../../general.css";
 import MainContentImage from "./MainContentImage/MainContentImage";
 import { useEffect, useState } from "react";
-
 function MainContent(props) {
   console.log("dwad", props.items);
 
@@ -31,7 +30,23 @@ function MainContent(props) {
               {props.items.Series_Title}{" "}
               <span>({props.items.Released_Year})</span>
             </h1>
-            <p class="imdb-rating">{props.items.IMDB_Rating}</p>
+            <div className="imdb-rating">
+              <p>{props.items.IMDB_Rating}</p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#f59f00"
+                viewBox="0 0 24 24"
+                strokeWidth={1}
+                stroke="#f59f00"
+                className="w-6 h-6 imdb-rating-svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                />
+              </svg>
+            </div>
           </div>
           <p class="director"> Directed by {props.items.Director}</p>
           <div class="image-and-data">
@@ -71,6 +86,85 @@ function MainContent(props) {
                   <span className="add-in-title">IMDb Votes</span> <br />
                   <span className="add-in-data">{props.items.No_of_Votes}</span>
                 </p>
+              </div>
+              <p className="stars-title">Stars</p>
+              <div className="stars-container">
+                <div className="stars star1">
+                  <div className="star-img">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1}
+                      stroke="#f76707"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                      />
+                    </svg>
+                  </div>
+                  <p className="stars-name">{props.items.Star1}</p>
+                </div>
+                <div className="stars star2">
+                  <div className="star-img">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1}
+                      stroke="#f76707"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                      />
+                    </svg>
+                  </div>
+                  <p className="stars-name">{props.items.Star2}</p>
+                </div>
+                <div className="stars star3">
+                  <div className="star-img">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1}
+                      stroke="#f76707"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                      />
+                    </svg>
+                  </div>
+                  <p className="stars-name">{props.items.Star3}</p>
+                </div>
+                <div className="stars star4">
+                  <div className="star-img">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1}
+                      stroke="#f76707"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                      />
+                    </svg>
+                  </div>
+                  <p className="stars-name">{props.items.Star4}</p>
+                </div>
               </div>
             </div>
           </div>
