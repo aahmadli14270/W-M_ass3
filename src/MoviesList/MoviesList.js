721f50b7746079893ba1668c52ff5e0cffc71564
aahmadli14270 <84677778+aahmadli14270@users.noreply.github.com>
2023-01-01 01:48:24 +0400

    moviesList v1

--- src/MoviesList/MoviesList.js
+++ src/MoviesList/MoviesList.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import "./MoviesList.css";
 
 function MoviesList(props) {
   const [data, setData] = useState([]);
@@ -16,15 +17,20 @@ function MoviesList(props) {
   return (
     <div>
       <p>Movies LIST</p>
-      {data.map((element) => (
-        <Link
-          key={element.ID}
-          to={"/InfoPage/" + element.Series_Title}
-          state={element}
-        >
-          <p> {element.Series_Title}</p>
-        </Link>
-      ))}
+      <div className="main-container-list">
+        {data.map((element) => (
+          <Link
+            key={element.ID}
+            to={"/InfoPage/" + element.Series_Title}
+            state={element}
+          >
+            <div className="item-container">
+              <img className="item-img" src={element.Poster_Link} />
+              <p> {element.Series_Title}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
